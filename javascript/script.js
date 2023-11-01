6783b9e38bbd52d636d194556f76e5afe066f886
LeandroUrnau <leandrournau@yahoo.com.br>
2023-11-01 09:10:20 -0300

    modal cadastro

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,10 @@ function salvarusuario(){
     email = document.getElementById("email").value;
     senha = document.getElementById("senha").value;
     localStorage.setItem(nome, '{"nome":"'+ nome +'", "email":"'+email+'", "senha":"'+senha+'"}')
-    alert("o usuario "+ nome +" foi salvo com sucesso!");
     console.log(localStorage.getItem(nome));
+    window.open('#modal-cadastro', '_self')
 }
+
+    function fechar(){
+        window.open('#','_self')
+    }
